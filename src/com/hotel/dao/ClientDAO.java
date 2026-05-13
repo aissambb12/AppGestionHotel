364f7e8126a87c6ef333492cfa891ac62a40e650
group <aissamboubrik@gmail.com>
2026-05-13 19:51:13 +0100

    update

--- src/com/hotel/dao/ClientDAO.java
+++ src/com/hotel/dao/ClientDAO.java
@@ -1,4 +1,13 @@
 package com.hotel.dao;
 
+import com.hotel.model.Client;
+import java.util.List;
+
 public interface ClientDAO {
+
+    void ajouter(Client client);
+    void modifier(Client client);
+    void supprimer(int idClient);
+    Client rechercherParId(int idClient);
+    List<Client> listerTous();
 }
