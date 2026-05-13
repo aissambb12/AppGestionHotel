a5669866bca51a3665ded694873a669491293775
group <aissamboubrik@gmail.com>
2026-05-13 16:28:20 +0100

    version3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import com.hotel.util.DatabaseConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         try {
             Connection connection = DatabaseConnection.getConnection();
@@ -17,5 +20,27 @@ public class Main {
             System.out.println("Erreur de connexion à la base de données !");
             e.printStackTrace();
         }
+
+        Connection conn = DatabaseConnection.getConnection() ;
+        try (
+                PreparedStatement ps = conn.prepareStatement("SELECT * FROM client");
+                ResultSet rs = ps.executeQuery()
+        ) {
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt("id_client") + " | " +
+                                rs.getString("nom") + " | " +
+                                rs.getString("prenom") + " | " +
+                                rs.getString("cin") + " | " +
+                                rs.getString("telephone") + " | " +
+                                rs.getString("email")
+                );
+            }
+
+        } catch (Exception e) {
+            System.out.println("Il y a un problème.");
+            e.printStackTrace();
+        }
+
     }
 }
