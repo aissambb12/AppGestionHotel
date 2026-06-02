f48c9ef44298c0d6c20943c516988834528413bc
DELL <zakariaelouahabi678@gmail.com>
2026-06-02 08:52:43 +0100

    Merge remote-tracking branch 'origin/master'

--- src/com/hotel/exception/ChambreNonDisponibleException.java
+++ src/com/hotel/exception/ChambreNonDisponibleException.java
@@ -1,9 +1,11 @@
 package com.hotel.exception;
 
-/*public class ChambreNonDisponibleException extends HotelException{
+import java.time.LocalDate;
+
+public class ChambreNonDisponibleException extends HotelException{
 
     public ChambreNonDisponibleException(String numeroChambre, LocalDate debut, LocalDate fin) {
         super("La chambre n° " + numeroChambre + " n'est pas disponible du " + debut + " au " + fin + ".");
     }
 
-}*/
+}
